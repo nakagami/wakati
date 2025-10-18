aedd3cd8b791bc8f0b3fcc94e0c99e883775ac24
Hajime Nakagami <nakagami@gmail.com>
2025-10-18 13:34:15 +0900

    add awabi

--- src/lib.rs
+++ src/lib.rs
@@ -19,10 +19,23 @@ use std::{
     sync::atomic::{AtomicBool, Ordering},
 };
 
+use awabi::tokenizer;
+
+struct WakatiState {
+    tokenizer: tokenizer::Tokenizer,
+}
+impl Default for WakatiState {
+    fn default() -> Self {
+        Self {
+            tokenizer: tokenizer::Tokenizer::new(None).unwrap(),
+        }
+    }
+}
+
 struct WakatiScalar {}
 
 impl VScalar for WakatiScalar {
-    type State = ();
+    type State = WakatiState;
 
     unsafe fn invoke(
         state: &Self::State,
@@ -51,11 +64,11 @@ impl VScalar for WakatiScalar {
     }
 }
 
-
 const EXTENSION_NAME: &str = env!("CARGO_PKG_NAME");
 
 #[duckdb_entrypoint_c_api()]
 pub unsafe fn extension_entrypoint(con: Connection) -> Result<(), Box<dyn Error>> {
+    //    let tokenizer = tokenizer::Tokenizer::new(None).expect("Can't find dictionary");
     con.register_scalar_function::<WakatiScalar>(EXTENSION_NAME)
         .expect("Failed to register wakati function");
     Ok(())
